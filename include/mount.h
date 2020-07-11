cd2571c5b9868551a37e7025cadcf9166e867b50
jpe230 <pablin.123.ra@gmail.com>
2020-07-11 00:32:02 -0500

    Fresh start
    🥵

--- include/mount.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOUNT_H_   /* Include guard */
-#define MOUNT_H_
-
-
-void unmountSaveData();
-int mountSaveData();
-
-
-#endif // TRANSLATIONS_H_
--- include/translations.h
+++
@@ -1,8 +0,0 @@
-#ifndef TRANSLATIONS_H_   /* Include guard */
-#define TRANSLATIONS_H_
-
-const char * translate(const char* rawName);
-int getIDNumber(const char* rawName);
-const char * translateMods(long int md);
-
-#endif // TRANSLATIONS_H_
